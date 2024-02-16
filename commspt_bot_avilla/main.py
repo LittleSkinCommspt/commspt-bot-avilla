fdf168d295d910d1ec73edb4d2a90c810405406d
SerinaNya <34389622+SerinaNya@users.noreply.github.com>
2024-02-16 20:31:06 +0800

    ✨ feat(pyproject): add script

--- commspt_bot_avilla/main.py
+++ commspt_bot_avilla/main.py
@@ -10,36 +10,38 @@ from loguru import logger
 from commspt_bot_avilla.utils.setting_manager import S_
 from rich.markdown import Markdown
 
-richuru.install()
 
-broadcast = it(Broadcast)
-saya = Saya(broadcast)
-saya.install_behaviours(BroadcastBehaviour(broadcast))
+def main():
+    richuru.install()
 
-# saya load modules
+    broadcast = it(Broadcast)
+    saya = Saya(broadcast)
+    saya.install_behaviours(BroadcastBehaviour(broadcast))
 
-logger.info("Loading Saya modules...")
+    # saya load modules
 
-with saya.module_context():
-    for module_info in pkgutil.iter_modules(["commspt_bot_avilla/modules"]):
-        logger.info(f"- module: {module_info.name}", rich=Markdown(f"# module: `{module_info.name}`"))
-        saya.require(f"modules.{module_info.name}")
-        logger.info("------", rich=Markdown("---"))
+    logger.info("Loading Saya modules...")
 
-# apply protocols
+    with saya.module_context():
+        for module_info in pkgutil.iter_modules(["commspt_bot_avilla/modules"]):
+            logger.info(f"- module: {module_info.name}", rich=Markdown(f"# module: `{module_info.name}`"))
+            saya.require(f"commspt_bot_avilla.modules.{module_info.name}")
+            logger.info("------", rich=Markdown("---"))
 
-avilla = Avilla()
-if S_.dev_mode:
-    from avilla.console.protocol import ConsoleProtocol
+    # apply protocols
 
-    avilla.apply_protocols(ConsoleProtocol())
-else:
-    from avilla.elizabeth.protocol import ElizabethConfig, ElizabethProtocol
+    avilla = Avilla()
+    if S_.dev_mode:
+        from avilla.console.protocol import ConsoleProtocol
 
-    config = ElizabethConfig(**S_.connection.model_dump())
-    logger.info(f"Protocol connection config: {config}")
-    avilla.apply_protocols(ElizabethProtocol().configure(config))
+        avilla.apply_protocols(ConsoleProtocol())
+    else:
+        from avilla.elizabeth.protocol import ElizabethConfig, ElizabethProtocol
 
-# finally launch
+        config = ElizabethConfig(**S_.connection.model_dump())
+        logger.info(f"Protocol connection config: {config}")
+        avilla.apply_protocols(ElizabethProtocol().configure(config))
 
-avilla.launch()
+    # finally launch
+
+    avilla.launch()
