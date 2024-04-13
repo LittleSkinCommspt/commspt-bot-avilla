8e89c23ada67b50bccf7deb80659ab8eeb2d613a
FalfaChino <falfachino@qq.com>
2024-04-13 18:56:15 +0800

    🐞 fix: `write_uid_db`

--- commspt_bot_avilla/utils/mongodb_manager.py
+++ commspt_bot_avilla/utils/mongodb_manager.py
@@ -4,7 +4,7 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from utils.setting_manager import S_
 
 
-async def write_uid_db(uid: int, qq: int):
+async def write_uid_db(uid: int | str, qq: int | str):
     # make sure int
     uid = int(uid) if not isinstance(uid, int) else uid
     qq = int(qq) if not isinstance(qq, int) else qq
@@ -21,18 +21,16 @@ async def write_uid_db(uid: int, qq: int):
         await coll.insert_one(r)
 
     mongo.close()
-    
+
 
 async def get_uid_db(qq: int | str) -> int | None:
     # make sure int
     qq = int(qq) if not isinstance(qq, int) else qq
-
 
     mongo = AsyncIOMotorClient(S_.db_mongo.url)
     coll = mongo["commspt-bot"]["uid"]
     i = await coll.find_one({"qq": qq})
 
     mongo.close()
-    
+
     return i["uid"] if i else None
-    
