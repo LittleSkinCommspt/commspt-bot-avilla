0871f8126b03f970b385bdd19354c1793a34c1bb
FalfaChino <falfachino@qq.com>
2024-04-14 00:01:53 +0800

    ✨ feat: add test

--- commspt_bot_avilla/modules/dev.py
+++ commspt_bot_avilla/modules/dev.py
@@ -3,9 +3,17 @@ from avilla.core.builtins.command import AvillaCommands
 from avilla.core import Context, MessageChain, Text, Selector, Notice, Message, MuteCapability
 from avilla.core.tools.filter import Filter
 
+from arclet.alconna import Alconna, Args, Arparma
+
 from commspt_bot_avilla.utils.setting_manager import S_
+from commspt_bot_avilla.utils.adv_filter import by_admin_only
 
 cmd = AvillaCommands(need_tome=False, remove_tome=True)
+
+
+default_dispatchers = [
+    Filter.cx.client.assert_true(by_admin_only()),
+]
 
 
 @cmd.on(
@@ -23,3 +31,10 @@ async def c1(cx: Context,target: Notice, duration: int):
     # )
     ...
 
+@cmd.on(Alconna(r"calltest", Args["arg1", str]["arg2", str]), dispatchers=default_dispatchers)
+async def _(cx: Context, target: Notice, parma: Arparma):
+    await cx.scene.send_message(f"Arg1: {parma["arg1"]}")
+    if parma["arg2"]:
+        await cx.scene.send_message(f"Arg2: right, {parma["arg2"]}")
+    else:
+        await cx.scene.send_message("Arg2: unknown")
