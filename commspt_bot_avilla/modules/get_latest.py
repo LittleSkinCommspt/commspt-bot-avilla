5b229d2d7dffa28e6d3a6b7fdb604ac5e806dece
Paper-white <69088493+Paper-white@users.noreply.github.com>
2024-12-13 17:45:05 +0800

    修改java.latest指令，添加默认参数arch:amd64

--- commspt_bot_avilla/modules/get_latest.py
+++ commspt_bot_avilla/modules/get_latest.py
@@ -48,11 +48,11 @@ async def _(ctx: Context, message: Message):
 @alcommand(
     Alconna(
         f"{S_.command_prompt}java.latest",
-        Args["version#Java 版本", int, 17]["type#Java 类型", str, "jre"]["os#操作系统类型", str, "windows"],
+        Args["version#Java 版本", int, 17]["type#Java 类型", str, "jre"]["os#操作系统类型", str, "windows"]["arch#架构",str,"amd64"],
         meta=CommandMeta(
             description="获取 Java 最新版本信息",
-            usage=f"{S_.command_prompt}java.latest [version] [type] [os]",
-            example=f"{S_.command_prompt}java.latest 17 jdk windows",
+            usage=f"{S_.command_prompt}java.latest [version] [type] [os] [arch]",
+            example=f"{S_.command_prompt}java.latest 17 jdk windows amd64",
         ),
     ),
 )
@@ -61,10 +61,12 @@ async def _(ctx: Context, message: Message, parma: Arparma):
     version: int = parma["version"]
     ftype: str = parma["type"]
     os: str = parma["os"]
+    arch: str = parma["arch"]
     java_latest: list[LibericaJavaLatest] = await LibericaJavaLatest.get(
         version_feature=version,
         bundle_type=f"{ftype}-full",
         os=os,
+        architecture=arch,
     )
     await ctx.scene.send_message(
         f"「Liberica Java {java_latest[0].feature_version} ({java_latest[0].bundle_type})」\n下载地址 > {java_latest[0].download_url_mirror}",
